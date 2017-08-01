010f86c4d9d548b38f8b69a90a27e11786cee24a
dave234 <daveoneill234@gmail.com>
2017-08-01 10:28:15 -0700

    Initial Commit

---
+++ AVAudioConnect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVAudioConnect
+//
+//  Created by David O'Neill on 8/1/17.
+//  Copyright © 2017 O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
